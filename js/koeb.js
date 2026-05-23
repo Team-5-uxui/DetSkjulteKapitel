d419b2ee64b4fc90005cde879ab66502728ee1ad
Laura von Deden <lavo0002@stud.ek.dk>
2026-05-23 23:25:43 +0200

    add køb feature

---
+++ js/koeb.js
@@ -0,0 +1,19 @@
+let selected = null;
+
+function selectPayment(val) {
+  // Nulstil alle
+  document.querySelectorAll(".radio-circle").forEach((el) => {
+    el.classList.remove("filled");
+  });
+  document.querySelectorAll(".radio-option").forEach((el) => {
+    el.style.border = "1px solid var(--primary-btn)";
+  });
+
+  if (selected === val) {
+    selected = null;
+  } else {
+    document.getElementById("circle-" + val).classList.add("filled");
+    document.getElementById("circle-" + val).closest(".radio-option").style.border = "2px solid #7c3db8";
+    selected = val;
+  }
+}
